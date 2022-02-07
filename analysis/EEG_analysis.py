f8fa21b8b051e9f773217b5d77e79c3d8d840322
Jakab Pilaszanovich <jakab.pilaszanovich@gmail.com>
2022-02-07 17:53:11 +0100

    Automated the reading and concatenation of multiple EEG header (.vhdr) files

--- analysis/EEG_analysis.py
+++ analysis/EEG_analysis.py
@@ -1,13 +1,21 @@
 import os
 import pathlib
+import glob
 import numpy as np
 import mne
 import matplotlib.pyplot as plt
+
 plt.style.use(['seaborn-colorblind', 'seaborn-darkgrid'])
 
 DIR = pathlib.Path(os.getcwd())
 
-sample_data_folder = '2022_02_04_pinknoise_jakab'
-sample_data_raw_file = os.path.join(DIR / sample_data_folder /'2022_02_04_pinknoise_jakab_1.vhdr')
+folder_name = '2022_02_04_pinknoise_jakab'
+folder_path = DIR / 'data' / folder_name
 
-raw = mne.io.read_raw_brainvision(sample_data_raw_file)
+header_files = folder_path.glob('*.vhdr')
+raw_files = []
+
+for idx, header_file in enumerate(header_files):
+    raw_files.append(mne.io.read_raw_brainvision(header_file))
+
+raw = mne.concatenate_raws(raw_files, verbose=True)
