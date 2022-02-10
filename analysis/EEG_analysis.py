8e9f7f5199725884a82c3fb79a6eb02b2ba2ab49
pfriedrich-hub <52865128+pfriedrich-hub@users.noreply.github.com>
2022-02-10 10:52:44 +0100

    add function to detect button press in time intervals

--- analysis/EEG_analysis.py
+++ analysis/EEG_analysis.py
@@ -10,7 +10,7 @@ plt.style.use(['seaborn-colorblind', 'seaborn-darkgrid'])
 DIR = pathlib.Path(os.getcwd())
 
 folder_name = '2022_02_04_pinknoise_jakab'
-folder_path = DIR / 'data' / folder_name
+folder_path = DIR / 'analysis' / 'data' / folder_name
 
 header_files = folder_path.glob('*.vhdr')
 raw_files = []
@@ -21,17 +21,22 @@ for idx, header_file in enumerate(header_files):
 raw = mne.concatenate_raws(raw_files, verbose=True)
 events = mne.events_from_annotations(raw)
 
+fs = raw.info['sfreq']
 # get hit rate for button presses
 # for every sweep onset, check if there was a button pressed within
 # a time window of ISI
-ISI = sr * 2
-sweep_times = events[np.where(events[:, 2] == 1)][:, 0]
-button_times = events[np.where(events[:, 2] == 7)][:, 0]
-hits = 0
-misses = 0
+ISI = fs * 2
+event_arr = events[0]
+event_idx = event_arr[:, 2]
+event_times = event_arr[:, 0]
+sweep_times = event_times[np.where(event_idx == 1)]
+button_times = event_times[np.where(event_idx == 7)]
+hits = []
+misses = []
 for sweep_time in sweep_times:
-    interval = sweep_time + 3 * sr # 3 sec interval after button press
-    if np.where(np.logical_and(button_times >= sweep_time, button_times <= interval)) is not None:
-        hits = +1
+    interval = sweep_time + ISI  # ISI (2 sec interval after button press)
+    button_in_interval = np.where(np.logical_and(button_times >= sweep_time, button_times <= interval))
+    if button_in_interval is not None:
+        hits.append(events[0])
     else:
         misses = +1
