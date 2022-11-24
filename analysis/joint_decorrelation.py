efb41802787a651be7fe839540a14f5a80f2cd97
Jakab Pilaszanovich <jakab.pilaszanovich@gmail.com>
2022-11-24 10:50:48 +0100

    Sorting keep dimensions after PCA

--- analysis/joint_decorrelation.py
+++ analysis/joint_decorrelation.py
@@ -30,8 +30,12 @@ def compute_transformation(epochs, conditions, keep, type="linear"):
     X = epochs.get_data().transpose(2, 1, 0)
     events = epochs.events
     idxs = [np.where(events[:, 2] == condition)[0] for condition in conditions]  # index of events per condition
-    to_jd1, from_jd1, _, pwr = dss1(X, keep1=keep)  # compute the JD transformations
+    to_jd1, from_jd1, pwr0, pwr1 = dss1(X)  # compute the JD transformations
     del X
+    to_jd1 = to_jd1[:, np.argsort(pwr1)[::-1]]  # sort them by magnitude
+    from_jd1 = from_jd1[np.argsort(pwr1)[::-1], :]
+    to_jd1 = to_jd1[:, 0:keep]  # only keep the largest ones
+    from_jd1 = from_jd1[0:keep, :]
     Y = apply_transform(epochs.get_data(), to_jd1)  # apply the unmixing matrix to get the components
     if type == "diff":
         D = Y[idxs[-1], :, :].mean(axis=0) - Y[idxs[0], :, :].mean(axis=0)  # take the difference between two conditions
@@ -43,8 +47,10 @@ def compute_transformation(epochs, conditions, keep, type="linear"):
     c1, nc1 = tscov(D)
     c0 /= nc0  # divide by total weight to normalize
     c1 /= nc1
-    to_jd2, from_jd2, _, pwr = dss0(c0, c1, keep1=keep)  # compute the transformations
-    return to_jd1, from_jd1, to_jd2, from_jd2, pwr
+    to_jd2, from_jd2, pwr0_biased, pwr1_biased = dss0(c0, c1)  # compute the transformations
+    to_jd2 = to_jd2[:, np.argsort(pwr1_biased)[::-1]]  # sort them by magnitude
+    from_jd2 = from_jd2[np.argsort(pwr1_biased)[::-1], :]
+    return to_jd1, from_jd1, to_jd2, from_jd2
 
 
 def get_epochs(subject_id, experiment="vocal_effort"):
@@ -59,11 +65,11 @@ def get_epochs(subject_id, experiment="vocal_effort"):
 
 
 def transform_epochs(epochs_from, epochs_to, conditions, keep, type="linear"):
-    to_jd1, from_jd1, to_jd2, from_jd2, pwr = compute_transformation(epochs_from, conditions, keep, type=type)
+    to_jd1, from_jd1, to_jd2, from_jd2 = compute_transformation(epochs_from, conditions, keep, type=type)
     Y = apply_transform(epochs_to.get_data(), [to_jd1, to_jd2, from_jd2, from_jd1])
     transformed_epochs = epochs_to.copy()
     transformed_epochs._data = Y
-    return transformed_epochs, to_jd1, from_jd1, to_jd2, from_jd2, pwr
+    return transformed_epochs, to_jd1, from_jd1, to_jd2, from_jd2
 
 
 def evoked_from_epochs(epochs):
@@ -84,36 +90,53 @@ ids_VE = list(name for name in os.listdir(VE_DIR) if os.path.isdir(os.path.join(
 ids_PN = list(name for name in os.listdir(PN_DIR) if os.path.isdir(os.path.join(PN_DIR, name)))
 ids = list(set(ids_VE) & set(ids_PN))
 condition_ids = [1, 2, 3, 4, 5]
-keep = 16
-evokeds, evokeds_avrgd = cfg["epochs"][f"event_id_{experiment}"].copy(), \
-                         cfg["epochs"][f"event_id_{experiment}"].copy()
+keep = 12
+evokeds_to, evokeds_jd, evokeds_avrgd_to, evokeds_avrgd_jd = \
+    cfg["epochs"][f"event_id_{experiment}"].copy(), \
+    cfg["epochs"][f"event_id_{experiment}"].copy(), \
+    cfg["epochs"][f"event_id_{experiment}"].copy(), \
+    cfg["epochs"][f"event_id_{experiment}"].copy(),
 for key in cfg["epochs"][f"event_id_{experiment}"]:
-    evokeds[key], evokeds_avrgd[key] = list(), list()
+    evokeds_to[key], evokeds_jd[key], evokeds_avrgd_to[key], evokeds_avrgd_to[key] = \
+        list(), list(), list(), list()
 channel_weights_all = []
 
 for id in ids:
     epochs_from = get_epochs(id, "vocal_effort")
     epochs_to = get_epochs(id, "pinknoise")
-    epochs_jd, to_jd1, from_jd1, to_jd2, from_jd2, pwr = transform_epochs(epochs_from, epochs_to, condition_ids, keep)
+    # epochs_to = epochs_from
+    epochs_jd, to_jd1, from_jd1, to_jd2, from_jd2 = transform_epochs(epochs_from, epochs_to, condition_ids, keep, type="diff")
 
     evoked_jd = evoked_from_epochs(epochs_jd)
     evoked_from = evoked_from_epochs(epochs_from)
     evoked_to = evoked_from_epochs(epochs_to)
     evoked_diff = [mne.combine_evoked([evoked_jd[i-1], evoked_to[i-1]], weights=[1, -1]) for i in condition_ids]
 
+    for condition in evoked_to:
+        evokeds_to[condition.comment].append(condition)
+        if len(evokeds_to[condition.comment]) == len(ids):
+            evokeds_avrgd_to[condition.comment] = mne.grand_average(
+                evokeds_to[condition.comment])
     for condition in evoked_jd:
-        evokeds[condition.comment].append(condition)
-        if len(evokeds[condition.comment]) == len(ids):
-            evokeds_avrgd[condition.comment] = mne.grand_average(
-                evokeds[condition.comment])
+        evokeds_jd[condition.comment].append(condition)
+        if len(evokeds_jd[condition.comment]) == len(ids):
+            evokeds_avrgd_jd[condition.comment] = mne.grand_average(
+                evokeds_jd[condition.comment])
     channel_weights = from_jd2 @ from_jd1
     channel_weights_all.append(channel_weights)
 
-mne.viz.plot_compare_evokeds(evoked_from, picks="FCz", title="JD from")
-mne.viz.plot_compare_evokeds(evoked_to, picks="FCz", title="JD to")
-mne.viz.plot_compare_evokeds(evoked_jd, picks="FCz", title="Transformed")
-mne.viz.plot_compare_evokeds(evoked_diff, picks="FCz", title="Diff")
+# mne.viz.plot_compare_evokeds(evoked_from, picks="FCz", title="JD from")
+# mne.viz.plot_compare_evokeds(evoked_to, picks="FCz", title="JD to")
+# mne.viz.plot_compare_evokeds(evoked_jd, picks="FCz", title="Transformed")
+# mne.viz.plot_compare_evokeds(evoked_diff, picks="FCz", title="Diff")
 
-mne.viz.plot_compare_evokeds(ignore_conds(evokeds_avrgd, "deviant", "button_press"), picks="FCz")
+# evoked_diff = [mne.combine_evoked([evokeds_avrgd_to[key], evokeds_avrgd_jd[key]], weights=[1, -1]) for i in enumerate(evokeds_avrgd_to) if i != "deviant"]
+
+mne.viz.plot_compare_evokeds(ignore_conds(evokeds_avrgd_to, "deviant", "button_press"), picks="FCz")
+mne.viz.plot_compare_evokeds(ignore_conds(evokeds_avrgd_jd, "deviant", "button_press"), picks="FCz")
+# mne.viz.plot_compare_evokeds(evoked_diff, picks="FCz")
 
 channel_weights_avrgd = np.asarray(channel_weights_all).mean(axis=0)
+mne.viz.plot_topomap(channel_weights_avrgd[0, :], epochs_jd.info)
+mne.viz.plot_topomap(channel_weights_avrgd[1, :], epochs_jd.info)
+mne.viz.plot_topomap(channel_weights_avrgd[2, :], epochs_jd.info)
