01cb39f4d50922a09b96d78ee4d1bb0dc746a2db
Jakab Pilaszanovich <jakab.pilaszanovich@gmail.com>
2023-08-03 14:54:34 +0200

    Update for time interval

--- analysis/plot_tools.py
+++ analysis/plot_tools.py
@@ -24,7 +24,7 @@ for idx, evoked in enumerate(evokeds):
     evoked.comment = 'distance/' + str(idx+1)
     evoked.shift_time(-0.2, relative=True)
     evoked.data = np.roll(evoked.data, 100, axis=1)
-    evoked.crop(tmin=-0.1, tmax=0.5)
+    evoked.crop(tmin=-0.2, tmax=0.5)
 combined_evokeds = mne.combine_evoked(evokeds, weights='equal')
 combined_evokeds.plot_joint(times=[0.096, 0.206, 0.335])
 mne.viz.plot_compare_evokeds(evokeds, colors=["#8ecae6", "#219ebc", "#023047", "#ffb703", "#fb8500"],
